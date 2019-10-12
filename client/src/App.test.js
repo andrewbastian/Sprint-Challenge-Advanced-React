b467a81260d7486e1c6ffdc548041458dc14c2f5
andrewbastian <andrew.bastian@gmail.com>
2019-10-12 11:43:38 -0700

    Answered study questions, MVP done.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+// no default export, so we're importing everyting from this library
+import * as rtl from "@testing-library/react";
+// not importing to a variable, since this just overrides jest global variables
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import PlayerCard from "./Components/PlayerCard.js"
+// this just allows react-testing-library to do some
+// routine cleanup after each test runs (to reset the DOM and such)
+afterEach(rtl.cleanup);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Render the heading", () => {
+  // render our React app into an in-memory DOM so we can test against it
+  const wrapper = rtl.render(<App />);
+
+  // element is now our dom element that we can test against
+  const element = wrapper.getByText(/player tracker/i);
+
+  // test will fail if element is not visible to our robot eyes
+  expect(element).toBeVisible();
+});
+
+test("PlayerCard Renders", () => {
+  const wrapper = rtl.render(<PlayerCard />);
+  const element = wrapper.getByText(/searches/i);
+  expect(element).toBeVisible();
 });
